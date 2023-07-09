2cdf8b9adbfbd60a6b4dccb9a04c34386f940fef
Abhishek Malvadkar <abhishek@dev.com>
2023-07-09 07:34:41 +0530

    How to read list values from properties file

---
+++ spring-boot-properties/src/main/java/in/spring/boot/properties/runner/ListPropertyTypeRunner.java
@@ -0,0 +1,28 @@
+package in.spring.boot.properties.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+public class ListPropertyTypeRunner implements CommandLineRunner {
+
+    @Value(value = "${app.notification.emails}")
+    private List<String> notificationEmails;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("... ListPropertyTypeRunner ...");
+
+        for (String notificationEmail : notificationEmails) {
+            log.info("NOTIFICATION EMAIL : {}", notificationEmail);
+        }
+
+        log.info("... ListPropertyTypeRunner ...");
+    }
+}
