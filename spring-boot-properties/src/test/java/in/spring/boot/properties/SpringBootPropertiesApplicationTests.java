6124302deb04257c3989536d291121c04962bc90
Abhishek Malvadkar <abhishek@dev.com>
2023-07-09 07:22:18 +0530

    Created spring boot project

---
+++ spring-boot-properties/src/test/java/in/spring/boot/properties/SpringBootPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package in.spring.boot.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
